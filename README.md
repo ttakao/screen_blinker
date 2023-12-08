20cfe90df8927827bc0d69526ee500748bc691eb
Tsukasa Takao <tsukasa.takao@gmail.com>
2023-12-08 23:55:51 +0900

    Almost correct

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 # screen_blinker
- This program can make blink Smartphone Screen as your like frequency.
+ This program can make blink Smartphone Screen as your like frequency and color.
  The objective of this program are;
  1. In dark place, you can use this program for watching your Aura. (Between fingers are good sample)
  2. You can empower the some special shape named sacred "geometry".
 
 # Environment
- This program should be put on a Web-site and access.
+ This program should be put on your Web-site and access on the browser. Supported browser is only Chrome now.
+ 
  
--- script.js
+++ script.js
@@ -1,17 +1,21 @@
+let blinkFreq=300
+let blinkColor = "#ffffff"
+let timerID
+
 function saveColor(){
   let colorDlg = document.getElementById('colorDialog')
   let color = document.getElementById('colorSelector').value
-  console.log(color)
+  blinkColor = color
   document.body.style.backgroundColor = color
-  
+  console.log("color:"+blinkColor)
   colorDlg.close()
 }
 
 function saveBlink(){
   let blinkDlg = document.getElementById('blinkDialog')
   let blink = document.getElementById('blinkSlider').value
-  console.log(blink)
-  
+  blinkFreq = blink
+  console.log("freq:"+blinkFreq)
   blinkDlg.close()
 }
 
@@ -20,6 +24,23 @@ function closeAbout(){
   aboutDlg.close()
 }
 
-function go_stop(){
-  alert("OK")
+function doBlink(){
+  document.body.style.backgroundColor = "#000000"
+
+  setTimeout( function(){
+    document.body.style.backgroundColor = blinkColor
+  }, blinkFreq)
+}
+function blinkStart(){
+  console.log("pushed")
+  button = document.getElementById("btnStart")
+  button.disabled = true
+  doBlink() 
+  timerID = setInterval(doBlink, blinkFreq)
+}
+
+function blinkStop(){
+  clearInterval(timerID)
+  button = document.getElementById("btnStart")
+  button.disabled = false
 }
